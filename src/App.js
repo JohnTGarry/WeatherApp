322be3409c919b2dabac312e7082fe963227704d
RivalGarry <johntgarry@gmail.com>
2022-04-08 19:41:56 +0100

    removing nodemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import './App.css'
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="icon-container">
+          <canvas id="icon" width="100" height="100"></canvas>
+        </div>
+      </div>
+      <div className="content">
+        <div className="general-information">
+          <div className="status">Enter A Location</div>
+          <div className="location">To Find The Weather</div>
+        </div>
+        <div className="detail-section">
+          <div className="detail">
+            <div className="title">Wind</div>
+            <div className="value">TBD</div>
+          </div>
+          <div className="detail">
+            <div className="title">Temperature</div>
+            <div className="value">TBD</div>
+          </div>
+          <div className="detail">
+            <div className="title">Precipitation</div>
+            <div className="value">TBD</div>
+          </div>
+        </div>
+        <div className="city-search-container">
+          <input type="text" className="city-search" placeholder="Enter Location" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
